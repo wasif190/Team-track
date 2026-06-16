be8824135cf397c304f0441cbf651e45ab2cfdc3
wasif190 <wasif.devs@gmail.com>
2026-06-17 00:32:33 +0530

    user schema

---
+++ src/models/user.model.js
@@ -0,0 +1,58 @@
+import mongoose, { Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    avatar: {
+      type: {
+        url: String,
+        localPath: String,
+      },
+      default: {
+        url: `https://placehold.co/200x200`,
+        localPath: '',
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      email: unique,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required!'],
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+    EmailVerificationEpiry: {
+      type: Date,
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model('Users', userSchema);
