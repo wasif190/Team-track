ac0608112059faa9d0eb27210b86ca37b1b7251d
wasif190 <wasif.devs@gmail.com>
2026-06-17 19:07:51 +0530

    validator for user registration

---
+++ src/validators/index.js
@@ -0,0 +1,31 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () => {
+    return [
+        body("email")
+            .trim()
+            .notEmpty()
+            .withMessage("Email is required")
+            .isEmail()
+            .withMessage("Email is invalid"),
+        body("username")
+            .trim()
+            .notEmpty()
+            .withMessage("Username is required")
+            .isLowercase()
+            .withMessage("Username must be in lowercase")
+            .isLength({min: 3})
+            .withMessage("Username must be atleast 3 characters"),
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("Password is required"),
+        body("fullName")
+            .optional()
+            .trim()
+    ]
+}
+
+export {
+    userRegisterValidator
+}
