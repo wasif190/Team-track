d99b70c39fc3f3bb0d8442b83a8a42efdc39c273
wasif190 <wasif.devs@gmail.com>
2026-06-17 23:05:41 +0530

    logout user

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
-import { login, registerUser } from '../controllers/auth.controller.js';
+import { login, logoutUser, registerUser } from '../controllers/auth.controller.js';
 import { validate } from '../middlewares/validator.middleware.js';
 import { userLoginValidator, userRegisterValidator } from '../validators/index.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
 router.route('/register').post(userRegisterValidator(), validate, registerUser);
 router.route('/login').post(userLoginValidator(), validate, login);
+router.route('/logout').post(verifyJWT, logoutUser);
 
 export default router;
