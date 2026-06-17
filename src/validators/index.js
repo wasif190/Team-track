9c7d2b33a40830a2fa7fa53e536b310d00550cd6
wasif190 <wasif.devs@gmail.com>
2026-06-17 19:34:15 +0530

    login validator

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,31 +1,36 @@
-import { body } from "express-validator";
+import { body } from 'express-validator';
 
 const userRegisterValidator = () => {
-    return [
-        body("email")
-            .trim()
-            .notEmpty()
-            .withMessage("Email is required")
-            .isEmail()
-            .withMessage("Email is invalid"),
-        body("username")
-            .trim()
-            .notEmpty()
-            .withMessage("Username is required")
-            .isLowercase()
-            .withMessage("Username must be in lowercase")
-            .isLength({min: 3})
-            .withMessage("Username must be atleast 3 characters"),
-        body("password")
-            .trim()
-            .notEmpty()
-            .withMessage("Password is required"),
-        body("fullName")
-            .optional()
-            .trim()
-    ]
-}
+  return [
+    body('email')
+      .trim()
+      .notEmpty()
+      .withMessage('Email is required')
+      .isEmail()
+      .withMessage('Email is invalid'),
+    body('username')
+      .trim()
+      .notEmpty()
+      .withMessage('Username is required')
+      .isLowercase()
+      .withMessage('Username must be in lowercase')
+      .isLength({ min: 3 })
+      .withMessage('Username must be atleast 3 characters'),
+    body('password').trim().notEmpty().withMessage('Password is required'),
+    body('fullName').optional().trim(),
+  ];
+};
 
-export {
-    userRegisterValidator
-}
+const userLoginValidator = () => {
+  return [
+    body('email')
+        .optional()
+        .isEmail()
+        .withMessage('Email is invalid'),
+    body("password")
+        .notEmpty()
+        .withMessage("Password is required")
+    ];
+};
+
+export { userRegisterValidator, userLoginValidator };
