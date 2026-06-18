ee77a6e66f38b66365040f4c82cd953a9f82a06f
wasif190 <wasif.devs@gmail.com>
2026-06-18 11:50:13 +0530

    auth routes

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,13 +1,46 @@
 import { Router } from 'express';
-import { login, logoutUser, registerUser } from '../controllers/auth.controller.js';
+import {
+  changeCurrentPassword,
+  forgotPasswordRequest,
+  getCurrentUser,
+  login,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  resendEmailVerification,
+  resetForgotPassword,
+  verifyEmail,
+} from '../controllers/auth.controller.js';
 import { validate } from '../middlewares/validator.middleware.js';
-import { userLoginValidator, userRegisterValidator } from '../validators/index.js';
+import {
+  userChangeCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  userLoginValidator,
+  userRegisterValidator,
+  userResetForgotPasswordValidator,
+} from '../validators/index.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
+// unsecured routes
 router.route('/register').post(userRegisterValidator(), validate, registerUser);
 router.route('/login').post(userLoginValidator(), validate, login);
+router.route('/verify-email/:verificationToken').get(verifyEmail);
+router.route('/refresh-token').post(refreshAccessToken);
+router
+  .route('/forgot-password')
+  .post(userForgotPasswordValidator(), validate, forgotPasswordRequest);
+router
+  .route('/reset-password/:resetToken')
+  .post(userResetForgotPasswordValidator, validate, resetForgotPassword);
+
+// secure routes - required verifyJWT
 router.route('/logout').post(verifyJWT, logoutUser);
+router.route('/corrent-user').post(verifyJWT, getCurrentUser);
+router
+  .route('/change-password')
+  .post(verifyJWT, userChangeCurrentPasswordValidator, validate, changeCurrentPassword);
+router.route('/resend-email-verification').post(verifyJWT, resendEmailVerification);
 
 export default router;
--- src/validators/index.js
+++ src/validators/index.js
@@ -33,4 +33,33 @@ const userLoginValidator = () => {
     ];
 };
 
-export { userRegisterValidator, userLoginValidator };
+const userChangeCurrentPasswordValidator = () => {
+    return [
+        body("oldPassword")
+            .notEmpty
+            .withMessage("Old password is required"),
+        body("newPassword")
+            .notEmpty()
+            .withMessage("New password is required")
+    ]
+}
+
+const userForgotPasswordValidator = () => {
+    return [
+        body("email")
+            .notEmpty()
+            .withMessage("Email is required")
+            .isEmail()
+            .withMessage("Email is invalid"),
+    ]
+}
+
+const userResetForgotPasswordValidator = () => {
+    return [
+        body("newPassword")
+            .notEmpty()
+            .withMessage("password is required")
+    ]
+}
+
+export { userRegisterValidator, userLoginValidator, userChangeCurrentPasswordValidator, userForgotPasswordValidator, userResetForgotPasswordValidator };
