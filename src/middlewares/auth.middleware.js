6e7e990272f76313f67aa487d65ccd74b2dc49cc
wasif190 <wasif.devs@gmail.com>
2026-06-21 23:01:28 +0530

    validate project permissions middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,3 +1,4 @@
+import { ProjectMember } from '../models/projectmember.model.js';
 import { User } from '../models/user.model.js';
 import { ApiError } from '../utils/apiError.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
@@ -23,6 +24,34 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     req.user = user;
     next();
   } catch (error) {
-    throw new ApiError(401, "Invalid access token")
+    throw new ApiError(401, 'Invalid access token');
   }
 });
+
+export const validateProjectPermission = (roles = []) => {
+  asyncHandler(async (req, res, next) => {
+    const { projectId } = req.params;
+
+    if (!projectId) {
+      throw new ApiError(400, 'Project id is missing');
+    }
+
+    const project = await ProjectMember.findOne({
+      project: new mongoose.Types.ObjectId(projectId),
+      user: new mongoose.Types.ObjectId(req.user._id),
+    });
+
+    if (!project) {
+      throw new ApiError(400, 'Project not found');
+    }
+
+    const givenRole = project?.role;
+    req.user.role = givenRole;
+
+    if (!roles.includes(givenRole)) {
+      throw new ApiError(403, 'You do not have permission to perform this action');
+    }
+
+    next();
+  });
+};
